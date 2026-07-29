d8230a8c966c0b6588b50d54d4892a89438886e2
Max19071 <fatihakin1907@gmail.com>
2026-07-29 16:39:32 +0300

    vite.config.ts içeriğini düzelt

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,8 @@
-/// <reference types="vite/client" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  base: "./",
+  plugins: [react(), tailwindcss()],
+});
