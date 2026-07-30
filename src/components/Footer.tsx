5624daded6bebb8f44330786b2aa1e876109b93e
Max19071 <fatihakin1907@gmail.com>
2026-07-30 19:53:35 +0300

    Adres bilgisini Liman Caddesi olarak düzelt

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -73,7 +73,7 @@ export default function Footer() {
                 <MapPin size={18} className="text-blue-500 mt-1 shrink-0" />
                 <div>
                   <span className="block text-white mb-1">Adres:</span>
-                  <span>Türkbükü Mahallesi, İnönü Caddesi, No: 101</span><br />
+                  <span>Türkbükü Mahallesi, Liman Caddesi, No: 101</span><br />
                   <span>Bodrum, Muğla / Türkiye</span>
                 </div>
               </li>
